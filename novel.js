5b3152c9f481771ec79395b6b67b6cced7082325
Rey <reyneraccel@gmail.com>
2018-11-05 19:51:57 -0200

    Update novel.js

--- novel.js
+++ novel.js
@@ -17,12 +17,27 @@ export class Novel {
         delete this.scenes[id];
     }
     
-    move (id) {
+    canMove (id) {
         const properties = this.scenes[id];
         const requirements = properties.requirements;
         const reqList = Object.keys(requirements);
+        const choices = Object.keys(this.scenes[this.scene].choices);
         const reqLen = reqList.length;
+        const choicesLen = choices.length;
         let canPass = true;
+        let hasChoice = false;
+        
+        for (let i = 0; i < choicesLen; i++) {
+            if (choices[i] === id) {
+                hasChoice = true;
+                break;
+            } else {
+                continue;
+            }
+        }
+        
+        if (!hasChoice) return false;
+        
         for (let i = 0; i < reqLen; i++) {
             const item = reqList[i];
             if (this.items[item] === undefined) {
@@ -35,6 +50,7 @@ export class Novel {
                 continue;
             }
         }
+        
         if (canPass) {
             this.scene = id;
             if (properties.rewards) {
@@ -52,9 +68,18 @@ export class Novel {
         }
     }
     
+    getChoices () {
+        return this.scenes[this.scene].choices;
+    }
+    
+    forceMove (id) {
+        this.scene = id;
+    }
+    
     moveRandom () {
         const choices = this.scenes[this.scene].choices;
-        this.scene = choices[Math.floor(Math.random() * choices.length)];
+        const choicesArray = Object.keys(choices);
+        this.scene = choicesArray[Math.floor(Math.random() * choices.length)];
     }
     
     getState () {
